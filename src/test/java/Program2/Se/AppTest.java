d3e45f8d99218c505fc1be5b66671cb5bd71c7ae
SaiPreethamReddyy <kadiri.saipreethamreddy@gmail.com>
2023-11-21 13:00:01 +0530

    first commit

---
+++ src/test/java/Program2/Se/AppTest.java
@@ -0,0 +1,20 @@
+package Program2.Se;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
